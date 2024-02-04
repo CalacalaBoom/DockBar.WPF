97f2e05e4243ee2f3765c9696b7aa9962f8425b0
zhangyu <zhangyu19990502@163.com>
2024-02-04 16:08:59 +0800

    添加图片

--- DockMenuPro/MainWindow.xaml.cs
+++ DockMenuPro/MainWindow.xaml.cs
@@ -26,15 +26,25 @@ namespace DockMenuPro
 
             imageList = new List<string>();
 
-            string[] files = Directory.GetFiles("E:\\Lib\\Demo\\Welcome\\DockMenuPro\\DockMenuPro\\Asset\\images");
-            foreach (var file in files)
-            {
-                if(System.IO.Path.GetExtension(file).Equals(".png",StringComparison.OrdinalIgnoreCase))
-                {
-                    imageList.Add(file);
-                }
-            }
+            //string[] files = Directory.GetFiles("E:\\Lib\\Demo\\Welcome\\DockMenuPro\\DockMenuPro\\Asset\\images");
+            //foreach (var file in files)
+            //{
+            //    if(System.IO.Path.GetExtension(file).Equals(".png",StringComparison.OrdinalIgnoreCase))
+            //    {
+            //        imageList.Add(file);
+            //    }
+            //}
             
+        }
+
+        private void Popup_Opened(object sender, EventArgs e)
+        {
+            var popup = (Popup)sender;
+            Dispatcher.BeginInvoke(new Action(() =>
+            {
+                double offset = (((Button)popup.Parent).ActualWidth - popup.Child.RenderSize.Width) / 2;
+                popup.HorizontalOffset = offset;
+            }), System.Windows.Threading.DispatcherPriority.Render);
         }
 
     }
